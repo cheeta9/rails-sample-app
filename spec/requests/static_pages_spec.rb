e931173f31ed4ba9543e187250f99f0d46a25772
tk-sakaguchi <takahiro_sakaguchi@tbn.co.jp>
2022-07-24 21:17:37 +0900

    add:applicationルートのルーティング

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,9 +3,16 @@ require 'rails_helper'
 RSpec.describe 'StaticPages', type: :request do
   let(:base_title) { 'Ruby on Rails Tutorial Sample App' }
 
+  describe 'GET /' do
+    it 'returns http success' do
+      get root_url
+      expect(response).to have_http_status(:success)
+    end
+  end
+
   describe 'GET /home' do
     it 'returns http success' do
-      get '/static_pages/home'
+      get static_pages_home_url
       expect(response).to have_http_status(:success)
       expect(response.body).to include("Home | #{base_title}")
     end
@@ -13,7 +20,7 @@ RSpec.describe 'StaticPages', type: :request do
 
   describe 'GET /help' do
     it 'returns http success' do
-      get '/static_pages/help'
+      get static_pages_help_url
       expect(response).to have_http_status(:success)
       expect(response.body).to include("Help | #{base_title}")
     end
@@ -21,7 +28,7 @@ RSpec.describe 'StaticPages', type: :request do
 
   describe 'GET /about' do
     it 'returns http success' do
-      get '/static_pages/about'
+      get static_pages_about_url
       expect(response).to have_http_status(:success)
       expect(response.body).to include("About | #{base_title}")
     end
