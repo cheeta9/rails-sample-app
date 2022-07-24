8ee205217f2f309ace0b87fac834b95b68f18699
tk-sakaguchi <takahiro_sakaguchi@tbn.co.jp>
2022-07-24 20:56:23 +0900

    fix:title表示ロジックの共通化

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,10 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'StaticPages', type: :request do
+  let(:base_title) { 'Ruby on Rails Tutorial Sample App' }
+
   describe 'GET /home' do
     it 'returns http success' do
       get '/static_pages/home'
       expect(response).to have_http_status(:success)
+      expect(response.body).to include("Home | #{base_title}")
     end
   end
 
@@ -12,6 +15,7 @@ RSpec.describe 'StaticPages', type: :request do
     it 'returns http success' do
       get '/static_pages/help'
       expect(response).to have_http_status(:success)
+      expect(response.body).to include("Help | #{base_title}")
     end
   end
 
@@ -19,6 +23,7 @@ RSpec.describe 'StaticPages', type: :request do
     it 'returns http success' do
       get '/static_pages/about'
       expect(response).to have_http_status(:success)
+      expect(response.body).to include("About | #{base_title}")
     end
   end
 end
