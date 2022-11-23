6e46a84627d36a261b9cc2287496bfae8d0579f7
TB-takahiro-sakaguchi <takahiro_sakaguchi@tbn.co.jp>
2022-11-23 22:42:44 +0900

    fix:名前付きルートを使うよう修正

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,15 +5,7 @@ RSpec.describe 'StaticPages', type: :request do
 
   describe 'GET /' do
     it 'returns http success' do
-      get root_url
-      expect(response).to have_http_status(:success)
-      assert_select 'title', base_title
-    end
-  end
-
-  describe 'GET /home' do
-    it 'returns http success' do
-      get static_pages_home_url
+      get root_path
       expect(response).to have_http_status(:success)
       assert_select 'title', base_title
     end
@@ -21,7 +13,7 @@ RSpec.describe 'StaticPages', type: :request do
 
   describe 'GET /help' do
     it 'returns http success' do
-      get static_pages_help_url
+      get help_path
       expect(response).to have_http_status(:success)
       assert_select 'title', "Help | #{base_title}"
     end
@@ -29,7 +21,7 @@ RSpec.describe 'StaticPages', type: :request do
 
   describe 'GET /about' do
     it 'returns http success' do
-      get static_pages_about_url
+      get about_path
       expect(response).to have_http_status(:success)
       assert_select 'title', "About | #{base_title}"
     end
@@ -37,7 +29,7 @@ RSpec.describe 'StaticPages', type: :request do
 
   describe 'GET /contact' do
     it 'returns http success' do
-      get static_pages_contact_url
+      get contact_path
       assert_response :success
       assert_select 'title', "Contact | #{base_title}"
     end
