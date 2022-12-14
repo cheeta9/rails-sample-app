a4cdc20c29cf5b6cc57568c23fefcb9f9e471ae6
TB-takahiro-sakaguchi <takahiro_sakaguchi@tbn.co.jp>
2022-12-14 23:47:50 +0900

    fix:テスト環境でhelperメソッドを使用する

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,35 +3,35 @@ require 'rails_helper'
 RSpec.describe 'StaticPages', type: :request do
   let!(:base_title) { 'Ruby on Rails Tutorial Sample App' }
 
-  describe 'GET /' do
-    it 'returns http success' do
+  describe '#home' do
+    it '正常にレスポンスを返すこと' do
       get root_path
       expect(response).to have_http_status(:success)
-      assert_select 'title', base_title
+      expect(response.body).to include base_title
     end
   end
 
-  describe 'GET /help' do
-    it 'returns http success' do
+  describe '#help' do
+    it '正常にレスポンスを返すこと' do
       get help_path
       expect(response).to have_http_status(:success)
-      assert_select 'title', "Help | #{base_title}"
+      expect(response.body).to include full_title('Help')
     end
   end
 
-  describe 'GET /about' do
-    it 'returns http success' do
+  describe '#about' do
+    it '正常にレスポンスを返すこと' do
       get about_path
       expect(response).to have_http_status(:success)
-      assert_select 'title', "About | #{base_title}"
+      expect(response.body).to include full_title('About')
     end
   end
 
-  describe 'GET /contact' do
-    it 'returns http success' do
+  describe '#contact' do
+    it '正常にレスポンスを返すこと' do
       get contact_path
       assert_response :success
-      assert_select 'title', "Contact | #{base_title}"
+      expect(response.body).to include full_title('Contact')
     end
   end
 end
